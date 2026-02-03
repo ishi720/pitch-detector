d35bc7b71e5c1edea2e20ad8d9b040c303143e40
ishi720 <ishizuka720@gmail.com>
2026-02-03 21:10:14 +0900

    Github pagesにデプロイ

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/pitch-detector' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
